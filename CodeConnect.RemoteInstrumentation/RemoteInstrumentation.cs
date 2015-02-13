b9c153f50b89de554afd5c8e18af131a98293346
Amadeus Wieczorek <git@amadeusw.com>
2015-02-12 20:49:20 -0800

    Additional checkin

---
+++ CodeConnect.RemoteInstrumentation/RemoteInstrumentation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeConnect.RemoteInstrumentation
+{
+    public abstract class _____RemoteInstrumentation
+    {
+        public abstract List<String> RunClientCode(string targetType, string targetMethod, System.Reflection.BindingFlags targetMethodFlags);
+
+        public static List<String> Logs // Injected
+        {
+            get; set;
+        }
+
+        public static T LogFunction<T>(string who, T what) // Injected
+        {
+            Logs.Add(who + " <- " + what.ToString());
+            return what;
+        }
+    }
+}
